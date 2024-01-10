39695858f8223110e6483de6e86c661745458122
Lisandra Tkaczuk <124847222+Lisatka@users.noreply.github.com>
2024-01-10 16:41:04 -0300

    Add files via upload

---
+++ SwitchCaseFinally.java
@@ -0,0 +1,26 @@
+
+    
+import java.util.Scanner;
+
+public class SwitchCaseFinally {
+    public static void main(String[] args) {
+        Scanner op = new Scanner(System.in);
+        System.out.println("Escolha sua classe meu caro aventureiro\n\r1 - Guerreiro\n\r2 - Ladrão\n\r3 - Mago");
+        int resp = op.nextInt();
+        switch (resp) {
+            case 1:
+                System.out.println("Você escolheu a classe Guerreiro");
+                break;
+            case 2:
+                System.out.println("Você escolheu a classe Ladrão");
+                break;
+            case 3:
+                System.out.println("Você escolheu a classe Mago");
+                break;
+            default:
+            System.out.println("Opção inválida.");
+                break;
+        }
+    }
+}
+
